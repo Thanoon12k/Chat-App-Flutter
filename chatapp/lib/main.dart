f7ef5984c13a2da6998a91ee3330a69bb824c062
Thanoon <engthanoon1@gmail.com>
2023-01-30 23:24:17 +0300

    profile complete name and desc befor image upload

--- chatapp/lib/main.dart
+++ chatapp/lib/main.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Profile());
+  runApp(MyApp());
 }
 
-class Profile extends StatelessWidget {
-  const Profile({ Key? key }) : super(key: key);
-
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+  final int x = 4;
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 25,
+          backgroundColor: Color.fromARGB(255, 211, 143, 65),
+          title: Text('دردشة شباب وبنات العراق'),
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 12,
+          ),
+        ),
+        body: Column(children: [
+          TextField(
+              decoration: InputDecoration(
+            labelText: "Your Name", //babel text
+            hintText: "اكتب اسمك هنا", //hint text
+            prefixIcon: Icon(Icons.person), //prefix iocn
+
+            hintStyle: TextStyle(
+                fontSize: 14, fontWeight: FontWeight.w100), //hint text style
+            labelStyle:
+                TextStyle(fontSize: 13, color: Color.fromARGB(255, 12, 12, 12)),
+          )),
+          TextField(
+              decoration: InputDecoration(
+            labelText: "Description", //babel text
+            hintText: "الوصف (اختياري )", //hint text
+            prefixIcon: Icon(Icons.person), //prefix iocn
+
+            hintStyle: TextStyle(
+                fontSize: 14, fontWeight: FontWeight.w100), //hint text style
+            labelStyle:
+                TextStyle(fontSize: 13, color: Color.fromARGB(255, 12, 12, 12)),
+          )),
+        ]),
+      ),
     );
   }
 }
--- chatapp/test/widget_test.dart
+++ chatapp/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:chatapp/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Profile());
+    await tester.pumpWidget( MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -27,4 +27,7 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+}
+
+class Profile {
 }
