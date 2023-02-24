b65b6a6e7397c45122a290be9ccc6801b76e0856
Thanoon Younes <engthanoon1@gmail.com>
2023-02-24 22:26:28 +0300

    complete register no api and sperate backend

--- backend/api/usersapi/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer,SerializerMethodField
-
-from roomsapp.models import Room
-from usersapp.models import Users
-
-class base_user_ser(ModelSerializer):
-    class Meta:
-        model=Users
-        fields=['id','name','email']
